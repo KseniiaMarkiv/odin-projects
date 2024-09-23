d0178f238c2aafba58808ee950d05c060cd6ca78
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-09-23 15:33:58 -0400

    Checked React feature without npm pkg

--- micro-reddit/config/importmap.rb
+++ micro-reddit/config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
