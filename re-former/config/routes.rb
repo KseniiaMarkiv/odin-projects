09de5f8c2ba7a385551db89964456fb00cd27f73
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-09-27 14:34:20 -0400

    Implemented update action and view

--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
