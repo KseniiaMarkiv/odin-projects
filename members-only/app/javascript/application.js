f8182cd1784623082ad8d9717e8b15978c00eddc
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-07 15:03:41 -0400

    Fixed Bootstrap bugs with popper

--- members-only/app/javascript/application.js
+++ members-only/app/javascript/application.js
@@ -1,8 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "bootstrap"
 import "stripe"
 import "controllers"
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
