14490d5a28cc35815a0b986c63341b3f460ecca2
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-07 19:57:32 -0400

    Fixed bug of don't display styles of Sign in page and partial links

--- members-only/app/controllers/application_controller.rb
+++ members-only/app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
+
 end
