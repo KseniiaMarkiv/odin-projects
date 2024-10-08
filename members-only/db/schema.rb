a1ea6c6ade93168cf424b14bf991e55335698332
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-08 17:11:31 -0400

    Created Post object

--- members-only/db/schema.rb
+++ members-only/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_162236) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_210736) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title", limit: 220
+    t.text "body", limit: 3000
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.text "email"
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_162236) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
