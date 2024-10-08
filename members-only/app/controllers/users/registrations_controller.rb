d488dceef335992e512061860aedb72176fea0c9
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-08 14:17:49 -0400

    Fixed bug in Update form on Edit page of User password

--- members-only/app/controllers/users/registrations_controller.rb
+++ members-only/app/controllers/users/registrations_controller.rb
@@ -1,2 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :html, :turbo_stream
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    user_path(resource) # Redirect to the users index after signup
+  end
+  def after_update_path_for(resource)
+    users_path # Redirect to the users index after updating
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redirect to login page after logout
+  end
 end
