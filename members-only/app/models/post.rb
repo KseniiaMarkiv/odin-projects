0dfd0e75c34a11139ec42ec7adf3bbd1149bcbb3
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-09 16:15:34 -0400

    Add errors validation for the Post object in the Model

--- members-only/app/models/post.rb
+++ members-only/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, :body, presence: true
+  validates :title, length: { in: 1..220 }
+  validates :body, length: { in: 10..3000 }
+
   belongs_to :user
 end
