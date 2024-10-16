bdb8574aec5cfdc5f56772e2b070f76cdec96399
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-16 19:23:07 -0400

    Added Action Text to Post

--- members-only/app/javascript/application.js
+++ members-only/app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "stripe"
 import "controllers"
+import "trix"
+import "@rails/actiontext"
--- members-only/app/models/post.rb
+++ members-only/app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :body, length: { in: 10..3000 }
 
   belongs_to :user
+  has_rich_text :body
 end
