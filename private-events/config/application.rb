2ab187ca33c938d478c30a18d3817a953c0d5535
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-29 16:03:35 -0400

    Create User object

--- private-events/config/application.rb
+++ private-events/config/application.rb
@@ -23,5 +23,9 @@ module PrivateEvents
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config/application.rb
+    config.time_zone = "Eastern Time (US & Canada)"
+
   end
 end
