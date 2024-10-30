51c1ee7ca901234be766d7cf47d3943e3d3efd09
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-10-30 15:48:33 -0400

    Added Devise view files for User obj

--- private-events/config/routes.rb
+++ private-events/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+
+  resources :users, only: [:index, :show]
 
   # Defines the root path route ("/")
   root "home#index"
