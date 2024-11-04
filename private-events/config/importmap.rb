99d91110678aacb9c64064d6e8412fc1b0c90e26
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-11-04 17:38:55 -0500

    Added Navbar

--- private-events/config/importmap.rb
+++ private-events/config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
+pin "navbar", to: "navbar.js"
 pin "jquery_check", to: "jquery_check.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
