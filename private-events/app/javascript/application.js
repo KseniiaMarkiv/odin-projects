f00be3c21dcb05d2faf42fea6bc211647ef6d36f
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-11-11 16:44:10 -0500

    Style show page

--- private-events/app/javascript/application.js
+++ private-events/app/javascript/application.js
@@ -6,3 +6,28 @@ import "navbar"
 import "jquery_check"
 
 import "controllers"
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex - 1].style.display = "block";  
+}
