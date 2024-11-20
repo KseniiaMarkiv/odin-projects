ae57086eb01f52f59c35ef96cc0d7d2f4fd33872
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-11-20 14:37:33 -0500

    Add validations to Event form

--- private-events/app/models/event.rb
+++ private-events/app/models/event.rb
@@ -3,7 +3,21 @@ class Event < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances, source: :user
 
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 } 
+  validates :date, presence: true 
+  validates :location, presence: true 
+  validates :description, presence: true, length: { minimum: 10 } 
+  
+  
   def absent_location
     location.present? ? location : "Location didn't point"
+  end
+  
+  private 
+  
+  def event_date_cannot_be_in_the_past 
+    if date.present? && date < DateTime.now 
+      errors.add(:date, "can't be in the past") 
+    end 
   end
 end
